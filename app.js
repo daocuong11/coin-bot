338633bd0617154aa877125b1a9c83e2010df0d2
DESKTOP-S7MBU16\hp <ldcuong11@gmail.com>
2021-03-29 21:55:57 +0700

    add schedule

--- app.js
+++ app.js
@@ -10,28 +10,30 @@ const binance = new Binance().options({
 const bot = new TelegramBot('1784948213:AAFFIuHqiI01aaOhGEg2m_Uq28CLbwL-I2E', { polling: true });
 
 let time_candlesticks = "1h";
-let chatId = "711570900";
+let chatId = "711570900";         
 
 let coin_symbols = ["SXPUSDT", "BNBUSDT"];
 
-/** Duyệt danh sách coin tính rsi */
-coin_symbols.forEach(coin_symbol => {
-  binance.candlesticks(coin_symbol, time_candlesticks, (error, ticks, symbol) => {
-    let prices = ticks.map(e => e[4]);
-    prices = prices.slice(0, -1);       // bỏ cây nến hiện tại
+const job = schedule.scheduleJob('* */1 * * *', function () {
+  /** Duyệt danh sách coin tính rsi */
+  coin_symbols.forEach(coin_symbol => {
+    binance.candlesticks(coin_symbol, time_candlesticks, (error, ticks, symbol) => {
+      let prices = ticks.map(e => e[4]);
+      prices = prices.slice(0, -1);       // bỏ cây nến hiện tại
 
-    let rsi14 = getRSI(prices, 14);
-    let rsi21 = getRSI(prices, 21);
+      let rsi14 = getRSI(prices, 14);
+      let rsi21 = getRSI(prices, 21);
 
-    let isProcess = (rsi14 > 70 && rsi21 > 70) || (rsi14 < 30 && rsi21 < 30);
+      let isProcess = (rsi14 > 70 && rsi21 > 70) || (rsi14 < 30 && rsi21 < 30);
 
-    console.info(`${symbol} rsi-14 ${rsi14} rsi-21 ${rsi21}`);
+      console.info(`${symbol} rsi-14 ${rsi14} rsi-21 ${rsi21}`);
 
-    if (isProcess) {
-      let msg = `${symbol} đang có entry đẹp rsi-14 ${rsi14} rsi-21 ${rsi21}`;
-      bot.sendMessage(chatId, msg);
-    }
-  }, { limit: 102 });
+      if (isProcess) {
+        let msg = `${symbol} đang có entry đẹp rsi-14 ${rsi14} rsi-21 ${rsi21}`;
+        bot.sendMessage(chatId, msg);
+      }
+    }, { limit: 102 });
+  });
 });
 
 function getRSI(prices, period) {
